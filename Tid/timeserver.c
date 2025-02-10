cb2f394e462b5f52b05f070a247c056632ba3a4f
hannescato <ditt@example.com>
2025-02-10 12:08:44 +0100

    first commit

---
+++ Tid/timeserver.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+
+#define TIME_PORT 37 //rfc 868 
+
+int main()
+{
+    
+}
