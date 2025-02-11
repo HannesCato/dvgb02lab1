136bd2e85cda657d75766a8c5402497f795ac1ea
hannescato <ditt@example.com>
2025-02-11 15:40:08 +0100

    timeserver

--- Tid/timeserver.c
+++ Tid/timeserver.c
@@ -5,8 +5,10 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <stdint.h>
 
-#define TIME_PORT 37 // RFC 868
+#define TIME_PORT 5000 // RFC 868
+#define TIME_DIFF 2208988800U
 
 int create_socket_udp() {
     int sock_fd;
@@ -18,10 +20,8 @@ int create_socket_udp() {
         exit(EXIT_FAILURE);
     }
 
-    //reset memory
     memset(&server_addr, 0, sizeof(server_addr));
 
-    //Adress and port
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;  
     server_addr.sin_port = htons(TIME_PORT);  
@@ -34,16 +34,36 @@ int create_socket_udp() {
 
     return sock_fd;
 }
+
 void wait_for_request(int sockfd, struct sockaddr_in *client_addr, socklen_t *client_len) {
     char buffer[1];  // Tom buffer (RFC 868 kräver ingen data i förfrågan)
 
     printf("Waiting for request...\n");
 
-    // Mottar en tom förfrågan från klienten
-    if (recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)client_addr, client_len) >= 0) {
-        printf("Received request from client\n");
+    ssize_t bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)client_addr, client_len);
+    
+    if (bytes_received >= 0) {
+        printf("Received request from client %s:%d\n",
+               inet_ntoa(client_addr->sin_addr), ntohs(client_addr->sin_port));
     } else {
         perror("Receive failed");
+    }
+}
+
+uint32_t get_time() {
+    time_t current_time = time(NULL);
+    uint32_t rfc868_time = (uint32_t)(current_time + TIME_DIFF);
+    return htonl(rfc868_time); // Konvertera till nätverksbyteordning (32-bit)
+}
+
+void send_time(int sockfd, struct sockaddr_in *client_addr, socklen_t client_len) {
+    uint32_t server_time = get_time();
+
+    if (sendto(sockfd, &server_time, sizeof(server_time), 0, (struct sockaddr *)client_addr, client_len) < 0) {
+        perror("Send failed");
+    } else {
+        printf("Sent time to client %s:%d -> %u (RFC 868 format)\n",
+               inet_ntoa(client_addr->sin_addr), ntohs(client_addr->sin_port), ntohl(server_time));
     }
 }
 
@@ -52,40 +72,14 @@ int main() {
     struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
 
-    printf("Socket skapades och är bunden till port %d.\n", TIME_PORT);
+    printf("Socket created and bound to port %d.\n", TIME_PORT);
 
-    // Vänta på en UDP-förfrågan
-    wait_for_request(sockfd, &client_addr, &client_len);
+    while (1) {
+        wait_for_request(sockfd, &client_addr, &client_len);
+
+        send_time(sockfd, &client_addr, client_len);
+    }
 
     close(sockfd);
     return 0;
 }
-
-
-
-/**
- */
-
-
-/*
-//wait for UDP request
-
-
-
- void get_time()
-{
- //get time in RFC-868 format
-}
-
-
-void send_time()
-{
-    //send time back to client
-}
-
-
-void run()
-{
-    //main loop
-}
-    */
