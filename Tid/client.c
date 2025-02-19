976e53186a90adc6b472e042c887deaec4a31bad
HugoLindahlSkog <hugoskog03@gmail.com>
2025-02-19 13:42:35 +0100

    comments

--- Tid/client.c
+++ Tid/client.c
@@ -11,28 +11,39 @@
 #define SERVER_IP "127.0.0.1"
 #define TIME_DIFF 2208988800U  // Sekunder mellan 1900 och 1970
 
+/*create a client socket with the arguments of sockaddr_in - the
+address to store the information of IPv4 and a pointer to the 
+server address */
 int create_client_socket(struct sockaddr_in *server_addr) {
     int sock_fd;
-
+/*create a socket with AF_INET - IPv4 and SOCK_DGRAM - 
+UDP protocol, 0 means no flags.
+If socket file descriptor returns -1, an error have occured*/
     sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock_fd < 0) {
         perror("Failed to create socket");
         exit(EXIT_FAILURE);
     }
-
+/* memset clears the server_addr structure, might contain random data
+that can lead to unspected behavior.*/
     memset(server_addr, 0, sizeof(*server_addr));
-    server_addr->sin_family = AF_INET;
-    server_addr->sin_port = htons(TIME_PORT);
+    server_addr->sin_family = AF_INET; //set address family to AF_INET / IPv4
+    server_addr->sin_port = htons(TIME_PORT); //set the serverport. htons converts the
+                                              //port from host byte order to network byte order
     
+/*inet_pton converts IP address to binary and stores it in server_addr
+if -1, error*/
     if (inet_pton(AF_INET, SERVER_IP, &server_addr->sin_addr) <= 0) {
         perror("Adressfel");
         close(sock_fd);
         exit(EXIT_FAILURE);
     }
-
+//else return socket file descriptor
     return sock_fd;
 }
-
+/* the client sends a request to the server
+sendto sockfd, no data is sent, message length is 0, 0 flags, server addres and the size of server aderess
+if -1, error occures*/
 void send_request(int sock_fd, struct sockaddr_in *server_addr) {
     if (sendto(sock_fd, NULL, 0, 0, (struct sockaddr*)server_addr, sizeof(*server_addr)) < 0) {
         perror("Failed to send request");
@@ -41,11 +52,11 @@ void send_request(int sock_fd, struct sockaddr_in *server_addr) {
     }
     printf("Request sent to server.\n");
 }
-
+/* After server recive request, server sends time that client recives 32 byte timestamp. */
 void receive_time(int sock_fd) {
-    uint32_t server_time;
-    socklen_t server_len = sizeof(struct sockaddr_in);
-    struct sockaddr_in server_addr;
+    uint32_t server_time; //stores time
+    socklen_t server_len = sizeof(struct sockaddr_in); //stores size of sockaddr_in, needed for recvfrom
+    struct sockaddr_in server_addr; //stores the server address information
 
     if (recvfrom(sock_fd, &server_time, sizeof(server_time), 0, (struct sockaddr*)&server_addr, &server_len) < 0) {
         perror("Failed to receive time");
@@ -53,11 +64,11 @@ void receive_time(int sock_fd) {
         exit(EXIT_FAILURE);
     }
 
-    server_time = ntohl(server_time);
+    server_time = ntohl(server_time); //converts network byte order to host byte order
 
-    time_t unix_time = (time_t)(server_time - TIME_DIFF);
+    time_t unix_time = (time_t)(server_time - TIME_DIFF); //converts from RFC 868 time to UNNIX time
 
-    printf("Time from server: %s", ctime(&unix_time));
+    printf("Time from server: %s", ctime(&unix_time)); //print the time
 }
 
 void run_client() {
--- Tid/timeserver.c
+++ Tid/timeserver.c
@@ -10,38 +10,61 @@
 #define TIME_PORT 37 // RFC 868
 #define TIME_DIFF 2208988800U
 
+/*create and set up a UDP socket for a server. This function 
+returns an interger which is the socket file descriptor sock_fd.
+AF_INET use IPv4, SOCK_DGRAM use UDP, 0 - let the system choose
+the appropriate protocol.
+If socket creation fails (sock_fd < 0) error occurs */
 int create_socket_udp() {
     int sock_fd;
     struct sockaddr_in server_addr;
-
+//sockaddr_in is structure used to store address information for IPv4
+//server_addr is a variable of type sockaddr_in
     sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock_fd < 0) {
         perror("Failed to create socket");
         exit(EXIT_FAILURE);
     }
-
+/* memset is used to clear the server_addr structure
+by setting all it bytes to 0.
+*/
     memset(&server_addr, 0, sizeof(server_addr));
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;  
-    server_addr.sin_port = htons(TIME_PORT);  
-
+    server_addr.sin_family = AF_INET; //specifies use of IPv4
+    server_addr.sin_addr.s_addr = INADDR_ANY; //allows socket to listen on all available networks
+    server_addr.sin_port = htons(TIME_PORT); 
+    //assigns a port number to the socket
+    //htons convert the port number from host byte order to network byte order
+    //TIME_PORT predefined port number 37
     if (bind(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Binding failed");
         close(sock_fd);
         exit(EXIT_FAILURE);
     }
-
+    //bind function connects sock_fd with the server_addr
+    //if bind fails, print error message
+    //close(sock_fd) is called to release socket before exiting
+    
     return sock_fd;
+    //returns socket file descriptor
 }
 
+
+/*  sockfd is the socket file descriptor
+    client_addr is a pointer to a sockaddr_in structure where
+    clients address will be stored.
+    client_len is a pointer to the size of client_addr, needed for recvfrom*/
 void wait_for_request(int sockfd, struct sockaddr_in *client_addr, socklen_t *client_len) {
     char buffer[1];  // Tom buffer (RFC 868 kräver ingen data i förfrågan)
 
     printf("Waiting for request...\n");
 
+// recvfrom() is used  to recieve UDP packets
     ssize_t bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)client_addr, client_len);
     
+// if bytes_received >= 0, client has sent a request
+//inet_ntoa converts clients IP address
+//ntohs converts clients port from network byte order to host byte order
     if (bytes_received >= 0) {
         printf("Received request from client %s:%d\n",
                inet_ntoa(client_addr->sin_addr), ntohs(client_addr->sin_port));
@@ -49,21 +72,24 @@ void wait_for_request(int sockfd, struct sockaddr_in *client_addr, socklen_t *cl
         perror("Receive failed");
     }
 }
-
+//generates a 32-bit timestamp that follows RFC 868
 uint32_t get_time() {
     time_t current_time = time(NULL);
+    //retreives the current time as the number of seconds since Jan 1, 1970
     uint32_t rfc868_time = (uint32_t)(current_time + TIME_DIFF);
     return htonl(rfc868_time); // Konvertera till nätverksbyteordning (32-bit)
 }
-
+// retrives time from get_time(), sends it via sendto()
 void send_time(int sockfd, struct sockaddr_in *client_addr, socklen_t client_len) {
     uint32_t server_time = get_time();
 
+// sends the 4 byte RFC 868 time value to the client
     if (sendto(sockfd, &server_time, sizeof(server_time), 0, (struct sockaddr *)client_addr, client_len) < 0) {
         perror("Send failed");
     } else {
         printf("Sent time to client %s:%d -> %u (RFC 868 format)\n",
                inet_ntoa(client_addr->sin_addr), ntohs(client_addr->sin_port), ntohl(server_time));
+               //converts
     }
 }
 
